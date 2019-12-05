2013f0d8a35e5e30733b89809e2907eb500af132
Diego Garcia <diego.ds.garcia@gmail.com>
2019-12-04 21:57:00 -0300

    Add mock of playlist

--- src/App.js
+++ src/App.js
@@ -3,12 +3,44 @@ import Player from './components/player'
 
 import Global from './styles/global'
 
-const playlist = [{
-  name: 'Kirby 64',
-  author: 'Nintendo 64',
-  image: './images/kirby.jpg',
-  source: './music/kirby.ogg'
-}]
+const playlist = [
+  {
+    name: 'Kirby 64',
+    author: 'Nintendo 64',
+    image: './images/kirby.jpg',
+    source: './music/kirby.ogg'
+  },
+  {
+    name: 'Castlevania Shyphonia of the Night',
+    author: 'PS1',
+    image: './images/castlevania.jpg',
+    source: './music/castlevania.ogg'
+  },
+  {
+    name: 'Mario 64',
+    author: 'Nintendo 64',
+    image: './images/mario-64.jpg',
+    source: './music/mario-64.ogg'
+  },
+  {
+    name: 'Mario Land',
+    author: 'Nintendo',
+    image: './images/mario-land.jpg',
+    source: './music/mario-land.ogg'
+  },
+  {
+    name: 'Pokemon',
+    author: 'Nintendo',
+    image: './images/pokemon.jpg',
+    source: './music/pokemon.ogg'
+  },
+  {
+    name: 'Zelda Ocarina of Time',
+    author: 'Nintendo 64',
+    image: './images/zelda.jpg',
+    source: './music/zelda.ogg'
+  },
+]
 
 function App() {
   const [{name, author, image, source}] = playlist
